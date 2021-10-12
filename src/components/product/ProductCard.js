b8ce400c668f143f9cab35ac4f1837c0183baaef
Samuel <samuelcatferrell@gmail.com>
2021-10-12 10:06:43 -0700

    fix(kandy-korner): Fixed issue when converting between dollars and cents

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -9,7 +9,8 @@ export const ProductCard = ({ product }) => {
                 <img src={product.imageUrl} alt={product.name} />
             </div>
             <div className="productData">
-                <h3 className="product__name">{product.productName}</h3>    
+                <h3 className="product__name">{product.productName}</h3>  
+                {/*  Check whether price needs to be measured in dollars or cents: */}
                 {product.price < 1 ? <h4 className="product__price">{product.price.toString().split('.')[1]}¢</h4> : <h4 className="product__price">${product.price}</h4>}    
                 <div className="product__type">Type: {product.productType.productType}</div>
                 <div className="product__location">
